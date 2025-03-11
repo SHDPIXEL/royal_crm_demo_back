19885810fc86d1f86f73d8ef4533801fe1ecf26c
SHDPIXEL <shdpixel@gmail.com>
2025-03-11 17:00:05 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 const {Sequelize} = require('sequelize');
 
-const sequelize = new Sequelize('royal_travel_crm','root','',{
-    host: 'localhost',
+const sequelize = new Sequelize('royal_crm_demo','admin','Hm5flOehaZba6lY5BzlU',{
+    host: 'royal-demo-crm.ch26co64cgxa.ap-south-1.rds.amazonaws.com',
     dialect: 'mysql'
 });
 
